6b4a3cd490ac493ec6677f7de978a8e1db8982cf
Rich Wilson <richard.wilson@smgdesign.org>
2020-09-03 00:05:58 +0100

    change public_id to id

--- mappers/user.js
+++ mappers/user.js
@@ -1,7 +1,7 @@
 class UserMapper {
     static toResponse(user) {
         return {
-            public_id: user.public_key,
+            id: user.public_key,
             username: user.username,
             email: user.email,
             fields: user.fields,
